7b5d4e8875ce26508f14434a3a3fdf6bd7f70a42
Anindo80 <anindo.biswas11@gmail.com>
2026-02-20 00:40:22 -0500

    Delete Code for Cleaner & Transformed Data directory

--- Code for Cleaner & Transformed Data/Cities.sql
+++
@@ -1,9 +0,0 @@
--- This query performs an INNER JOIN between the customer_info and location_info tables 
--- to retrieve each customer's resident city 
-
-SELECT 
-	c.customer_id,
-	city
-FROM customer_info AS c
-JOIN location_data AS l
-ON c.customer_id = l.customer_id;
